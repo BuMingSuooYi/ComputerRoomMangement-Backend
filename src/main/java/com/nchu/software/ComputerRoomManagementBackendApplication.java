9eada41a3ce75b5e8137f289d41cef7526613f69
BuMingSuooYi <2980293201@qq.com>
2023-06-05 15:17:14 +0800

    first commit

---
+++ src/main/java/com/nchu/software/ComputerRoomManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nchu.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerRoomManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerRoomManagementBackendApplication.class, args);
+	}
+
+}
